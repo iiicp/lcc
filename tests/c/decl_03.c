29fbf846c3f5bb482a62caecde590cbbd7f79600
蔡鹏 <caipeng@insta360.com>
2023-02-07 12:34:49 +0800

    add test case

---
+++ tests/c/decl_03.c
@@ -0,0 +1,51 @@
+/// symbol LONG in the typedef define in global scope
+typedef long LONG;
+
+/// symbol LONG in the global scope
+int LONG;
+
+enum E{
+/// symbol LONG in the global scope
+LONG,
+};
+
+/// symbol S in the struct/enum tag scope
+struct S{
+/// symbol LONG in the local(variable) scope
+int LONG;
+};
+
+/// symbol AAA in the struct/enum tag scope
+enum AAA{
+/// symbol AAA in the global scope, does not conflict with tag scope
+AAA
+};
+
+/// symbol main in the global scope
+int main() {
+/// symbol LONG is the typedef define in local(variable) scope
+typedef long LONG;
+    
+enum {
+/// symbol LONG in the local(variable) scope, some as character a and front typedef(LONG)
+LONG
+};
+    
+/// symbol a in the local(variable) sope
+int a = 4;
+
+/// search symbol a in the label scope
+goto a;
+/// symbol a in the label scope
+a:
+return 0;
+}
+
+
+/// summary
+/// 1, global scope (typedef)
+/// 2, local scope (struct declaration)
+/// 3, struct/enum tag scope
+/// 4, label scope
+///
+/// Symbol conflict, you need to search in the current scope
---
+++ tests/c/decl_04.c
@@ -0,0 +1,21 @@
+char g1[] = "hello,world";
+short g2[100];
+const int g3;
+unsigned g4[];
+signed long g5[2][3];
+long long g6[][2][3];
+int test(int *a[], float f, double d) {
+
+}
+
+void test2(int **a, float *f, double d) {
+  if (1) {
+
+  }else {
+
+  }
+}
+
+void test3() {}
+
+double test4(int a, float, double d);

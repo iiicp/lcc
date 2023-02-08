35b048b748931a9a9d2f673c227a9e5ecaccb497
蔡鹏 <caipeng@insta360.com>
2023-02-08 09:55:05 +0800

    add test case

--- tests/c/decl_05.c
+++ tests/c/decl_05.c
@@ -1,3 +1,33 @@
 /// abstract declarator
 /// 1, param type list (in function declaration)
 /// 2, type name (in case-expr, in sizeof-expr, in post-fix-expr)
+
+struct S {
+  int a,b:3;
+  long c;
+};
+
+float f(int, float, struct S s);
+
+typedef struct Person{
+  const char *name;
+  int age;
+}Person;
+
+Person constructPerson(const char *name, int age);
+
+Person copyPerson(Person other);
+
+Person copyPerson(Person other) {
+  Person  p;
+  p.name = other.name;
+  p.age = other.age;
+  return p;
+}
+
+Person *copyPerson2(Person *other) {
+  Person  *p = (Person *)malloc(sizeof(Person));
+  p->name = other->name;
+  p->age = other->age;
+  return p;
+}

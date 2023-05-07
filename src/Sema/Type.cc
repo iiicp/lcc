9722b3b9ed44c65a834ec167a9716da205a38345
iiicp <iiicp@outlook.com>
2023-05-07 22:05:27 +0800

    add type add expression node

--- src/Sema/Type.cc
+++ src/Sema/Type.cc
@@ -108,9 +108,9 @@ PrimitiveType::PrimitiveType(Kind kind) {
   }
 }
 
-Type PrimitiveType::create(bool isConst, bool isVolatile,
-                           PrimitiveType::Kind kind) {
-  return Type(isConst, isVolatile, PrimitiveType(kind));
+std::shared_ptr<Type> PrimitiveType::create(bool isConst, bool isVolatile,
+                                            PrimitiveType::Kind kind) {
+  return std::make_shared<Type>(isConst, isVolatile, PrimitiveType(kind));
 }
 
 bool PrimitiveType::operator==(const PrimitiveType &rhs) const {
@@ -124,11 +124,11 @@ bool PrimitiveType::operator!=(const PrimitiveType &rhs) const {
 PointerType::PointerType(bool isRestricted, std::shared_ptr<Type> elementType)
     : restricted_(isRestricted), elementType_(elementType) {}
 
-Type PointerType::create(bool isConst, bool isVolatile, bool restricted,
-                         Type elementType) {
-  return Type(
-      isConst, isVolatile,
-      PointerType(restricted, std::make_shared<Type>(std::move(elementType))));
+std::shared_ptr<Type> PointerType::create(bool isConst, bool isVolatile,
+                                          bool restricted,
+                                          std::shared_ptr<Type> elementType) {
+  return std::make_shared<Type>(isConst, isVolatile,
+                                PointerType(restricted, elementType));
 }
 
 bool PointerType::operator==(const PointerType &rhs) const {
@@ -140,11 +140,71 @@ bool PointerType::operator!=(const PointerType &rhs) const {
   return !(rhs == *this);
 }
 
-Type FunctionType::create(Type returnType, std::vector<Argument> &&arguments,
-                          bool lastIsVararg) {
-  return Type(false, false,
-              FunctionType(std::make_shared<Type>(std::move(returnType)),
-                           std::move(arguments), lastIsVararg));
+uint64_t PointerType::sizeOf() const { return 8; }
+
+uint64_t PointerType::alignOf() const { return 8; }
+
+ArrayType::ArrayType(bool isRestricted, bool isStatic,
+                     std::shared_ptr<Type> type, size_t size)
+    : isRestricted_(isRestricted), isStatic_(isStatic), type_(type),
+      size_(size) {}
+
+std::shared_ptr<Type> ArrayType::create(bool isConst, bool isVolatile,
+                                        bool isRestricted, bool isStatic,
+                                        std::shared_ptr<Type> elemType,
+                                        size_t size) {
+  return std::make_shared<Type>(
+      isConst, isVolatile, ArrayType(isRestricted, isStatic, elemType, size));
+}
+
+uint64_t ArrayType::sizeOf() const { return size_ * type_->sizeOf(); }
+
+uint64_t ArrayType::alignOf() const { return type_->alignOf(); }
+
+bool ArrayType::operator==(const ArrayType &rhs) const {
+  return std::tie(isRestricted_, *type_, size_) ==
+         std::tie(rhs.isRestricted_, *rhs.type_, rhs.size_);
+}
+
+bool ArrayType::operator!=(const ArrayType &rhs) const {
+  return !(rhs == *this);
+}
+
+AbstractArrayType::AbstractArrayType(bool isRestricted,
+                                     std::shared_ptr<Type> type)
+    : isRestricted_(isRestricted), type_(type) {}
+
+std::shared_ptr<Type>
+AbstractArrayType::create(bool isConst, bool isVolatile, bool isRestricted,
+                          std::shared_ptr<Type> elemType) {
+  return std::make_shared<Type>(isConst, isVolatile,
+                                AbstractArrayType(isRestricted, elemType));
+}
+
+uint64_t AbstractArrayType::sizeOf() const { LCC_UNREACHABLE; }
+
+uint64_t AbstractArrayType::alignOf() const { return type_->alignOf(); }
+
+bool AbstractArrayType::operator==(const lcc::AbstractArrayType &rhs) const {
+  return std::tie(isRestricted_, *type_) ==
+         std::tie(rhs.isRestricted_, *rhs.type_);
+}
+
+bool AbstractArrayType::operator!=(const lcc::AbstractArrayType &rhs) const {
+  return !(rhs == *this);
+}
+
+FunctionType::FunctionType(std::shared_ptr<Type> returnType,
+                           std::vector<Argument> arguments, bool lastIsVararg)
+    : returnType_(returnType), arguments_(std::move(arguments)),
+      lastIsVararg_(lastIsVararg) {}
+
+std::shared_ptr<Type> FunctionType::create(std::shared_ptr<Type> returnType,
+                                           std::vector<Argument> &&arguments,
+                                           bool lastIsVararg) {
+  return std::make_shared<Type>(
+      false, false,
+      FunctionType(returnType, std::move(arguments), lastIsVararg));
 }
 
 bool FunctionType::operator==(const FunctionType &rhs) const {
@@ -164,6 +224,92 @@ bool FunctionType::operator==(const FunctionType &rhs) const {
 bool FunctionType::operator!=(const FunctionType &rhs) const {
   return !(rhs == *this);
 }
+
+StructType::StructType(std::string_view name, uint64_t index)
+    : name_(name), index_(index) {}
+
+std::shared_ptr<Type> StructType::create(bool isConst, bool isVolatile,
+                                         std::string_view name,
+                                         uint64_t index) {
+  return std::make_shared<Type>(isConst, isVolatile, StructType(name, index));
+}
+
+uint64_t StructType::sizeOf() const {}
+
+uint64_t StructType::alignOf() const {}
+
+bool StructType::operator==(const lcc::StructType &rhs) const { return false; }
+
+bool StructType::operator!=(const lcc::StructType &rhs) const {
+  return !(rhs == *this);
+}
+
+UnionType::UnionType(std::string_view name, uint64_t index)
+    : name_(name), index_(index) {}
+
+std::shared_ptr<Type> UnionType::create(bool isConst, bool isVolatile,
+                                        std::string_view name, uint64_t index) {
+  return std::make_shared<Type>(isConst, isVolatile, UnionType(name, index));
+}
+
+uint64_t UnionType::sizeOf() const {}
+
+uint64_t UnionType::alignOf() const {}
+
+bool UnionType::operator==(const lcc::UnionType &rhs) const { return false; }
+
+bool UnionType::operator!=(const lcc::UnionType &rhs) const {
+  return !(rhs == *this);
+}
+
+EnumType::EnumType(std::string_view name, uint64_t index)
+    : name_(name), index_(index) {}
+
+std::shared_ptr<Type> EnumType::create(bool isConst, bool isVolatile,
+                                       std::string_view name, uint64_t index) {
+  return std::make_shared<Type>(isConst, isVolatile, EnumType(name, index));
+}
+
+uint64_t EnumType::sizeOf() const {}
+
+uint64_t EnumType::alignOf() const {}
+
+bool EnumType::operator==(const lcc::EnumType &rhs) const { return false; }
+
+bool EnumType::operator!=(const lcc::EnumType &rhs) const {
+  return !(rhs == *this);
+}
+
+std::shared_ptr<Type> AnonymousStructType::create(bool isConst, bool isVolatile,
+                                                  std::uint64_t id,
+                                                  std::vector<Field> fields,
+                                                  std::uint32_t sizeOf,
+                                                  std::uint32_t alignOf) {
+  return std::make_shared<Type>(
+      isConst, isVolatile,
+      AnonymousStructType(id, std::move(fields), sizeOf, alignOf));
+}
+
+std::shared_ptr<Type> AnonymousUnionType::create(bool isConst, bool isVolatile,
+                                                 std::uint64_t id,
+                                                 std::vector<Field> fields,
+                                                 std::uint32_t sizeOf,
+                                                 std::uint32_t alignOf) {
+  return std::make_shared<Type>(
+      isConst, isVolatile,
+      AnonymousUnionType(id, std::move(fields), sizeOf, alignOf));
+}
+
+std::shared_ptr<Type> AnonymousEnumType::create(bool isConst, bool isVolatile,
+                                                std::uint64_t id,
+                                                std::shared_ptr<Type> type) {
+  return std::make_shared<Type>(isConst, isVolatile,
+                                AnonymousEnumType(id, type));
+}
+
+uint64_t AnonymousEnumType::sizeOf() const { return type_->sizeOf(); }
+
+uint64_t AnonymousEnumType::alignOf() const { return type_->alignOf(); }
 
 std::uint64_t Type::alignOf() const {
   return match(type_, [&](auto &&value) -> uint64_t {
--- src/Sema/Type.h
+++ src/Sema/Type.h
@@ -54,7 +54,7 @@ private:
   PrimitiveType(Kind kind);
 
 public:
-  static Type create(bool isConst, bool isVolatile, Kind kind);
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile, Kind kind);
   DECL_GETTER(Kind, kind);
   DECL_GETTER(uint64_t, sizeOf);
   DECL_GETTER(uint64_t, alignOf);
@@ -65,20 +65,68 @@ public:
   bool operator!=(const PrimitiveType &rhs) const;
 };
 
+class ArrayType final {
+private:
+  std::shared_ptr<Type> type_;
+  size_t size_;
+  bool isRestricted_;
+  bool isStatic_;
+  ArrayType(bool isRestricted, bool isStatic, std::shared_ptr<Type> type,
+            size_t size);
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      bool isRestricted, bool isStatic,
+                                      std::shared_ptr<Type> elemType,
+                                      size_t size);
+
+  DECL_GETTER(std::shared_ptr<Type>, type);
+  DECL_GETTER(size_t, size);
+  DECL_GETTER(bool, isRestricted);
+  DECL_GETTER(bool, isStatic);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
+
+  bool operator==(const ArrayType &rhs) const;
+  bool operator!=(const ArrayType &rhs) const;
+};
+
+class AbstractArrayType final {
+private:
+  std::shared_ptr<Type> type_;
+  bool isRestricted_;
+  AbstractArrayType(bool isRestricted, std::shared_ptr<Type> type);
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      bool isRestricted,
+                                      std::shared_ptr<Type> elemType);
+  DECL_GETTER(std::shared_ptr<Type>, type);
+  DECL_GETTER(bool, isRestricted);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
+
+  bool operator==(const AbstractArrayType &rhs) const;
+  bool operator!=(const AbstractArrayType &rhs) const;
+};
+
 class PointerType final {
 private:
   std::shared_ptr<Type> elementType_;
   bool restricted_{false};
-  uint64_t sizeOf_{8};
-  uint64_t alignOf_{8};
   PointerType(bool isRestricted, std::shared_ptr<Type> elementType);
 
 public:
-  static Type create(bool isConst, bool isVolatile, bool restricted, Type elementType);
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      bool restricted,
+                                      std::shared_ptr<Type> elementType);
   DECL_GETTER(std::shared_ptr<Type>, elementType);
-  DECL_GETTER(uint64_t, sizeOf);
-  DECL_GETTER(uint64_t, alignOf);
   DECL_GETTER(bool, restricted);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
 
   bool operator==(const PointerType &rhs) const;
   bool operator!=(const PointerType &rhs) const;
@@ -89,17 +137,18 @@ private:
   std::shared_ptr<Type> returnType_;
   std::vector<std::pair<std::shared_ptr<Type>, std::string_view>> arguments_;
   bool lastIsVararg_;
-public:
-  using Argument = std::pair<std::shared_ptr<Type>, std::string_view>;
-private:
-  FunctionType(std::shared_ptr<Type> returnType,
-               std::vector<Argument> arguments, bool lastIsVararg)
-      : returnType_(returnType), arguments_(std::move(arguments)),
-        lastIsVararg_(lastIsVararg) {}
 
 public:
-  static Type create(Type returnType,
-                     std::vector<Argument>&& arguments, bool lastIsVararg);
+  using Argument = std::pair<std::shared_ptr<Type>, std::string_view>;
+
+private:
+  FunctionType(std::shared_ptr<Type> returnType,
+               std::vector<Argument> arguments, bool lastIsVararg);
+
+public:
+  static std::shared_ptr<Type> create(std::shared_ptr<Type> returnType,
+                                      std::vector<Argument> &&arguments,
+                                      bool lastIsVararg);
 
   DECL_GETTER(std::shared_ptr<Type>, returnType);
   DECL_GETTER(bool, lastIsVararg);
@@ -112,10 +161,172 @@ public:
   [[nodiscard]] bool operator!=(const FunctionType &rhs) const;
 };
 
+class StructType final {
+private:
+  std::string_view name_;
+  uint64_t index_;
+  StructType(std::string_view name, uint64_t index);
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      std::string_view name, uint64_t index);
+
+  DECL_GETTER(std::string_view, name);
+  DECL_GETTER(uint64_t, index);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
+
+  bool operator==(const StructType &rhs) const;
+  bool operator!=(const StructType &rhs) const;
+};
+
+class UnionType final {
+private:
+  std::string_view name_;
+  uint64_t index_;
+  UnionType(std::string_view name, uint64_t index);
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      std::string_view name, uint64_t index);
+
+  DECL_GETTER(std::string_view, name);
+  DECL_GETTER(uint64_t, index);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
+
+  bool operator==(const UnionType &rhs) const;
+  bool operator!=(const UnionType &rhs) const;
+};
+
+class EnumType final {
+private:
+  std::string_view name_;
+  uint64_t index_;
+  EnumType(std::string_view name, uint64_t index);
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      std::string_view name, uint64_t index);
+
+  DECL_GETTER(std::string_view, name);
+  DECL_GETTER(uint64_t, index);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
+
+  bool operator==(const EnumType &rhs) const;
+  bool operator!=(const EnumType &rhs) const;
+};
+
+struct Field {
+  std::shared_ptr<Type> type;
+  std::string_view name;
+  std::optional<std::pair<uint32_t, uint32_t>> bitFieldBounds;
+  size_t layoutIndex;
+
+  bool operator==(const Field &rhs) const {
+    return std::tie(type, name, bitFieldBounds) ==
+           std::tie(rhs.type, rhs.name, rhs.bitFieldBounds);
+  }
+  bool operator!=(const Field &rhs) const { return !(rhs == *this); }
+};
+
+class AnonymousStructType final {
+private:
+  uint32_t id_;
+  std::vector<Field> fields_;
+  uint64_t sizeOf_;
+  uint64_t alignOf_;
+  AnonymousStructType(std::uint64_t id, std::vector<Field> &&fields,
+                      std::uint32_t sizeOf, std::uint32_t alignOf)
+      : id_(id), fields_(std::move(fields)), sizeOf_(sizeOf),
+        alignOf_(alignOf) {}
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      std::uint64_t id,
+                                      std::vector<Field> fields,
+                                      std::uint32_t sizeOf,
+                                      std::uint32_t alignOf);
+
+  DECL_GETTER(uint32_t, id);
+  DECL_GETTER(std::vector<Field>, fields);
+  DECL_GETTER(uint64_t, sizeOf);
+  DECL_GETTER(uint64_t, alignOf);
+
+  bool operator==(const AnonymousStructType &rhs) const {
+    return id_ == rhs.id_;
+  }
+  bool operator!=(const AnonymousStructType &rhs) const {
+    return !(rhs == *this);
+  }
+};
+
+class AnonymousUnionType final {
+private:
+  uint32_t id_;
+  std::vector<Field> fields_;
+  uint64_t sizeOf_;
+  uint64_t alignOf_;
+  AnonymousUnionType(std::uint64_t id, std::vector<Field> &&fields,
+                     std::uint32_t sizeOf, std::uint32_t alignOf)
+      : id_(id), fields_(std::move(fields)), sizeOf_(sizeOf),
+        alignOf_(alignOf) {}
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      std::uint64_t id,
+                                      std::vector<Field> fields,
+                                      std::uint32_t sizeOf,
+                                      std::uint32_t alignOf);
+
+  DECL_GETTER(uint32_t, id);
+  DECL_GETTER(std::vector<Field>, fields);
+  DECL_GETTER(uint64_t, sizeOf);
+  DECL_GETTER(uint64_t, alignOf);
+
+  bool operator==(const AnonymousUnionType &rhs) const {
+    return id_ == rhs.id_;
+  }
+  bool operator!=(const AnonymousUnionType &rhs) const {
+    return !(rhs == *this);
+  }
+};
+
+class AnonymousEnumType final {
+private:
+  std::shared_ptr<Type> type_;
+  uint64_t id_;
+  AnonymousEnumType(uint64_t id, std::shared_ptr<Type> type)
+      : id_(id), type_(type) {}
+
+public:
+  static std::shared_ptr<Type> create(bool isConst, bool isVolatile,
+                                      std::uint64_t id,
+                                      std::shared_ptr<Type> type);
+
+  DECL_GETTER(uint64_t, id);
+  DECL_GETTER(std::shared_ptr<Type>, type);
+
+  uint64_t sizeOf() const;
+  uint64_t alignOf() const;
+
+  bool operator==(const AnonymousEnumType &rhs) const { return id_ == rhs.id_; }
+  bool operator!=(const AnonymousEnumType &rhs) const {
+    return !(rhs == *this);
+  }
+};
+
 class Type final {
 public:
   using Variant =
-      std::variant<std::monostate, PrimitiveType, PointerType, FunctionType>;
+      std::variant<std::monostate, PrimitiveType, ArrayType, AbstractArrayType,
+                   StructType, UnionType, EnumType, AnonymousStructType,
+                   AnonymousUnionType, AnonymousEnumType, PointerType,
+                   FunctionType>;
 
 private:
   Variant type_;
@@ -131,6 +342,15 @@ public:
   [[nodiscard]] bool isUndefined() const {
     return std::holds_alternative<std::monostate>(type_);
   }
+
+  DECL_GETTER(Variant, type);
+  DECL_GETTER(std::string_view, name);
+  DECL_GETTER(bool, isConst);
+  DECL_GETTER(bool, isVolatile);
+
+  void setName(std::string_view name) { name_ = name; }
+
+  bool isTypedef() const { return !name_.empty(); }
 
   [[nodiscard]] std::uint64_t sizeOf() const;
   [[nodiscard]] std::uint64_t alignOf() const;
